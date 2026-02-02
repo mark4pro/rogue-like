06bc8c775f31728ee03c6214677162308dfebfe2
James <ps4jamesmrichard@gmail.com>
2026-02-01 21:54:52 -0500

    start of the world gen (working)

--- worldGen/scripts/worldgen.gd
+++ worldGen/scripts/worldgen.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var chunkSize : Vector2i = Vector2i(10, 10)
+var tileSize : int = 32
 var thisSeed : int = -1 # -1 use random seed
 
 var hasWorldNode : bool = false
@@ -26,27 +27,45 @@ enum Biome {
 
 var biomeMap = [] # biome_map[chunk_y][chunk_x]
 var world = [] # world[y][x] = TileType
+var layers = {}
+
+func genLayers(parent: Node2D) -> Dictionary:
+	var newLayers = {}
+
+	var ground : TileMapLayer = TileMapLayer.new()
+	ground.name = "Ground"
+	ground.z_index = 0
+	parent.add_child(ground)
+	newLayers.ground = ground
+
+	var props : TileMapLayer = TileMapLayer.new()
+	props.name = "Props"
+	props.z_index = 1
+	parent.add_child(props)
+	newLayers.props = props
+
+	return newLayers
+
 
 func initArrays() -> void:
 	biomeMap.clear()
-	for y in chunkSize.y:
+	for y in range(chunkSize.y):
 		biomeMap.append([])
-		for x in chunkSize.x:
+		for x in range(chunkSize.x):
 			biomeMap[y].append(Biome.FOREST)
-	
+
 	var world_w = chunkSize.x * 10
 	var world_h = chunkSize.y * 10
 
 	world.clear()
-	for y in world_h:
+	for y in range(world_h):
 		world.append([])
-		for x in world_w:
+		for x in range(world_w):
 			world[y].append(TileType.EMPTY)
 
-
 func genBiome() -> void:
-	for cy in chunkSize.x:
-		for cx in chunkSize.y:
+	for cy in range(chunkSize.y):
+		for cx in range(chunkSize.x):
 			var n = noise.get_noise_2d(cx, cy)
 			if n < -0.3:
 				biomeMap[cy][cx] = Biome.CAVE
@@ -62,16 +81,51 @@ func genCave() -> void:
 	pass
 
 func genForest() -> void:
-	pass
-	#if noise.get_noise_2d(x, y) > 0.4:
-		#world[y][x] = TREE
+	for cy in range(chunkSize.y):
+		for cx in range(chunkSize.x):
+			if biomeMap[cy][cx] != Biome.FOREST:
+				continue
+				
+			for y in range(10):
+				for x in range(10):
+					var wx = cx * 10 + x
+					var wy = cy * 10 + y
+					
+					world[wy][wx] = TileType.GRASS
+					
+					var n = noise.get_noise_2d(wx, wy)
+					if n > 0.35:
+						world[wy][wx] = TileType.TREE
 
 func mixGen() -> void:
 	pass
 
 #gens the actual map in the tileMapLayer
 func mapGen() -> void:
-	pass
+	var ground : TileMapLayer = layers.ground
+	var props  : TileMapLayer = layers.props
+	
+	var tileSet = load("res://Assets/tilesets/forest_test.tres")
+	
+	ground.tile_set = tileSet
+	props.tile_set = tileSet
+	
+	ground.tile_set.tile_size = Vector2i(tileSize, tileSize)
+	props.tile_set.tile_size = Vector2i(tileSize, tileSize)
+	
+	ground.clear()
+	props.clear()
+	
+	for y in range(world.size()):
+		for x in range(world[y].size()):
+			match world[y][x]:
+				TileType.GRASS:
+					ground.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
+					
+				TileType.TREE:
+					ground.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
+					props.set_cell(Vector2i(x, y), 0, Vector2i(1, 0))
+
 
 func _ready() -> void:
 	var seed : int = randi()
@@ -84,11 +138,15 @@ func _process(delta: float) -> void:
 	var isTestEnv : Node2D = get_tree().root.get_node_or_null("TestGen")
 	if isTestEnv:
 		if not hasWorldNode:
-			var newWorldNode : TileMap = TileMap.new()
+			var newWorldNode : Node2D = Node2D.new()
 			newWorldNode.name = "World"
 			isTestEnv.add_child(newWorldNode)
+			layers = genLayers(newWorldNode)
 			hasWorldNode = true
 		if not loaded:
 			initArrays()
 			genBiome()
+			genForest()
+			print(world[0][0])
+			mapGen()
 			loaded = true
