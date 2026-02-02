a31b02e36ad376fc5948d6507b7e996b1de68dce
AcidGhost175 <malachimiller5050@gmail.com>
2026-02-01 19:25:40 -0600

    yfkytkty
    sfa

--- Script/player_controller.gd
+++ Script/player_controller.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 @onready var sprint_timer = $Timer
+@onready var cooldown_timer = $cooldown
+
 
 @export var sprint_speed : float = 15000
 @export var walk_speed : float = 7000
@@ -11,12 +13,14 @@ func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("sprint"):
+	if Input.is_action_just_pressed("sprint") and cooldown_timer.time_left == 0:
 		speed = sprint_speed
 		sprint_timer.start()
 	if Input.is_action_just_released("sprint"):
 		speed = walk_speed
 		sprint_timer.stop()
+		cooldown_timer.start()
+	
 	
 	var dir : Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	
@@ -25,4 +29,9 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	speed = walk_speed
-	print("test")
+	cooldown_timer.start()
+
+
+func _on_cooldown_timeout() -> void:
+	pass
+	
