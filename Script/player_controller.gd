f6dba7586ce6aca865965ef44a4c56d34a74be7f
James <ps4jamesmrichard@gmail.com>
2026-02-02 21:45:11 -0500

    f

--- Script/player_controller.gd
+++ Script/player_controller.gd
@@ -34,7 +34,6 @@ func _physics_process(delta: float) -> void:
 	if linear_velocity != Vector2.ZERO and not roll:
 		$Sprite2D.play("walk")
 	
-	
 	if Input.is_action_just_pressed("roll") and not roll and can_roll:
 		roll = true
 		can_roll = false
