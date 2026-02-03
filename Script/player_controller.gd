350e4bf6e76179ddd049d03c04dd1bd1e2c7feec
AcidGhost175 <malachimiller5050@gmail.com>
2026-02-02 18:41:19 -0600

    hfdh
    htfh

--- Script/player_controller.gd
+++ Script/player_controller.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 @onready var sprint_timer = $Timer
 @onready var cooldown_timer = $cooldown
-
+var roll : bool = false
 
 @export var sprint_speed : float = 15000
 @export var walk_speed : float = 7000
@@ -24,6 +24,16 @@ func _physics_process(delta: float) -> void:
 	var dir : Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	
 	linear_velocity = dir * speed * delta;
+	
+	
+	if Input.is_action_just_pressed("roll") and not roll:
+		roll = true
+	if roll:
+		$Sprite2D.rotation += 7 * delta
+	
+	if $Sprite2D.rotation%PI/2 == 1:
+		$Sprite2D.rotation = 0
+		roll = false 
 	
 	if linear_velocity.x < 0:
 		$Sprite2D.flip_h = true
