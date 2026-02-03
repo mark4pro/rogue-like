41058079a626b8ac010ae5fac1f0e077fa2766ae
AcidGhost175 <malachimiller5050@gmail.com>
2026-02-02 20:20:20 -0600

    fasdf
    faaf

--- Script/player_controller.gd
+++ Script/player_controller.gd
@@ -19,19 +19,27 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("sprint") and cooldown_timer.time_left == 0:
 		speed = sprint_speed
+		$Sprite2D.speed_scale = 3
 		sprint_timer.start()
 	if Input.is_action_just_released("sprint"):
 		speed = walk_speed
+		$Sprite2D.speed_scale = 1
 		sprint_timer.stop()
 		cooldown_timer.start()
 	
 	var dir : Vector2 = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	
 	linear_velocity = dir * speed * delta;
+	
+	if linear_velocity != Vector2.ZERO and not roll:
+		$Sprite2D.play("walk")
+	
+	
 	if Input.is_action_just_pressed("roll") and not roll and can_roll:
 		roll = true
 		can_roll = false
 	if roll:
+		$Sprite2D.stop()
 		$Sprite2D.rotation += 7 * delta
 	$Sprite2D.rotation_degrees = int($Sprite2D.rotation_degrees) % 360
 	if $Sprite2D.rotation_degrees == 0 and roll:
@@ -46,6 +54,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	speed = walk_speed
+	$Sprite2D.speed_scale = 1
 	cooldown_timer.start()
 
 
