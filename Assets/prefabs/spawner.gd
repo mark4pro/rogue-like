ea84fc51bef488708f45a52ecdddadb262767c47
AcidGhost175 <malachimiller5050@gmail.com>
2026-02-12 19:53:50 -0600

    enemy spawner

---
+++ Assets/prefabs/spawner.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+#things to use when we have multiple enemies
+#var enemies = [preload("res://e1.tscn"), preload("res://e2.tscn")]
+#var enemy = enemies.pick_random().instantiate()
+
+@export var enemy_scene : PackedScene = preload("res://Assets/prefabs/enemy.tscn")
+@export var max_enemies : int = 10
+var current_enemies : int = 0
+
+func _on_timer_timeout():
+	if current_enemies < max_enemies and Global.sceneIndex > 0:
+		var enemy = enemy_scene.instantiate()
+	
+		var spawn_x = randf_range(0, get_viewport_rect().size.x)
+		var spawn_y = -50
+	
+		enemy.position = Vector2(spawn_x, spawn_y)
+		add_child(enemy)
