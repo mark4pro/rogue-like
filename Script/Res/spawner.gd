74c222d3ce061339612c6976de7670664582dd52
AcidGhost175 <malachimiller5050@gmail.com>
2026-02-15 14:09:19 -0600

    still cant fix the camera issue

--- Script/Res/spawner.gd
+++ Script/Res/spawner.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-#things to use when we have multiple enemies
-#var enemies = [preload("res://e1.tscn"), preload("res://e2.tscn")]
-
-
 @export var enemy_scene = [preload("res://Assets/prefabs/enemy.tscn")]
 @export var max_enemies : int = 10
 var current_enemies : int = 0
@@ -14,6 +10,7 @@ func _on_timer_timeout():
 	
 		var spawn_x = randf_range(0, get_viewport_rect().size.x)
 		var spawn_y = -50
-	
+		
 		enemy.position = Vector2(spawn_x, spawn_y)
+		current_enemies += 1
 		add_child(enemy)
---
+++ Script/enemy.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+@export var damage : float = 0.5
+
+var touching : bool = false
+
+func _process(delta: float) -> void:
+	var bodies = $Area2D.get_overlapping_bodies()
+	for b in bodies:
+		if b.has_method("take_damage"): b.take_damage(damage)
