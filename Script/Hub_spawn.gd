cae14bc2f5f719312e257cfd3de7c3a83d3c8956
James <ps4jamesmrichard@gmail.com>
2026-04-15 04:45:56 -0400

    updated spawn script

--- Script/Hub_spawn.gd
+++ Script/Hub_spawn.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+@export var spawn : Marker2D
+@export var flipPlayer : bool = true
+
 func _process(_delta: float) -> void:
-	if not Global.player:
+	if not Global.player and spawn:
 		Global.saveGame()
 		var newPlayer : RigidBody2D = Global.playerRes.instantiate()
-		newPlayer.global_position = $Spawn.global_position
+		newPlayer.global_position = spawn.global_position
 		get_tree().current_scene.add_child(newPlayer)
+		newPlayer.rot_point.scale.x = -1 if flipPlayer else 1
